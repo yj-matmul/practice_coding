764f64f17ee6c224f8841d3be1769bd4ab6a8447
yeongjae-yu <yyj3053@naver.com>
2019-04-01 22:50:16 +0900

    20190401 yyj work

---
+++ algorithm/554_brick_wall/brick_wall_yyj.py
@@ -0,0 +1,22 @@
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        # 각 brick이 끝나는 지점의 index를 저장
+        for n in range(len(wall)):
+            for idx in range(1, len(wall[n])):
+                wall[n][idx] += wall[n][idx - 1]
+
+        blank = {}  # 각 열마다 벽돌 사이 개수를 담을 hash
+        for n in range(len(wall)):
+            # 벽돌 맨 끝은 제외
+            for idx in wall[n][:-1]:
+                if idx in blank.keys():
+                    blank[idx] += 1
+                else:
+                    blank[idx] = 1
+
+        max_blank = 0  # 전체 열 중 벽돌 사이가 가장 빈 곳을 담을 변수
+        for key in blank.keys():
+            max_blank = max(max_blank, blank[key])
+
+        # 전체 층에서 벽돌 사이가 가장 많이 빈 곳을 빼줌
+        return len(wall) - max_blank
