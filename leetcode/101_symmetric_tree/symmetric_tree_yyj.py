6dce1796384de758a9596a0262d87c419a0bc66c
yyj3053 <yyj3053@gmail.com>
2020-08-13 18:03:56 +0900

    20200813 solving 101 problem

---
+++ leetcode/101_symmetric_tree/symmetric_tree_yyj.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root: return True
+
+        left = []
+        right = []
+
+        if root.left:
+            left = self.leftInOrder(root.left, left)
+        if root.right:
+            right = self.rightInOrder(root.right, right)
+
+        if len(left) != len(right): return False
+
+        for i in range(len(left)):
+            if left[i] != right[i]: return False
+
+        return True
+
+    def leftInOrder(self, node, vals):
+        vals.append(node.val)
+        if node.left:
+            vals = self.leftInOrder(node.left, vals)
+
+        if node.right:
+            vals = self.rightInOrder(node.right, vals)
+
+        return vals
+
+    def rightInOrder(self, node, vals):
+        vals.append(node.val)
+        if node.right:
+            vals = self.rightInOrder(node.right, vals)
+        if node.left:
+            vals = self.leftInOrder(node.left, vals)
+
+        return vals
